450c85813e27ebe37cce93ec4acba4976fa385d3
PatrickYanZ <37357319+PatrickYanZ@users.noreply.github.com>
2022-07-17 09:40:55 -0400

    fix the bug and add the bss

--- tests/vehicle/test_behavior.py
+++ tests/vehicle/test_behavior.py
@@ -3,6 +3,7 @@ import pytest
 from highway_env.vehicle.objects import Obstacle
 from highway_env.road.road import Road, RoadNetwork
 from highway_env.vehicle.behavior import IDMVehicle, LinearVehicle
+from highway_env.vehicle.kinematics import Vehicle
 
 FPS = 15
 vehicle_types = [IDMVehicle, LinearVehicle]
@@ -11,15 +12,21 @@ vehicle_types = [IDMVehicle, LinearVehicle]
 @pytest.mark.parametrize("vehicle_type", vehicle_types)
 def test_stop_before_obstacle(vehicle_type):
     road = Road(RoadNetwork.straight_road_network(lanes=1))
-    vehicle = vehicle_type(road=road, position=[0, 0], speed=20, heading=0)
+    # vehicle = vehicle_types(road=road, position=[0, 0], speed=20, heading=0)
+    vehicle = Vehicle(road=road, position=[0, 0], speed=20, heading=0)
     obstacle = Obstacle(road=road, position=[80, 0])
     road.vehicles.append(vehicle)
     road.objects.append(obstacle)
     for _ in range(10 * FPS):
         road.act()
         road.step(dt=1/FPS)
+    print(vehicle.position[0],vehicle.position[1])
+    print(obstacle.position[0],obstacle.position[1])
     assert not vehicle.crashed
     assert vehicle.position[0] == pytest.approx(obstacle.position[0] - vehicle_type.DISTANCE_WANTED, abs=1)
     assert vehicle.position[1] == pytest.approx(0)
     assert vehicle.speed == pytest.approx(0, abs=1)
     assert vehicle.heading == pytest.approx(0)
+
+if __name__ == "__main__":
+    test_stop_before_obstacle(vehicle_types)
